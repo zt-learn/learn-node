8ad5c6413a5c89851c9d275b774d59d2026f30d6
zhentaoo <1904974327@qq.com>
2015-04-07 12:48:44 +0800

    NodeJS基础学习

---
+++ export/hello.js
@@ -0,0 +1,8 @@
+exports.add = function (a, b) {
+    return a + b;
+}
+
+exports.world = function () {
+    console.log('hello world');
+}
+
---
+++ export/test.js
@@ -0,0 +1,6 @@
+var hello = require('./hello');
+
+var a = 1;
+var b = 23;
+
+console.log(hello.add(a, b));
