5d277c073ff186f39a11e2387737ff4e3c462199
zhentaoo <1904974327@qq.com>
2015-05-04 11:01:27 +0800

    function匿名函数的使用，模块的使用

--- modules/hello.js
+++ modules/hello.js
@@ -1,11 +1,11 @@
 exports.add = function (a, b) {
     return a + b;
-}
+};
 
 exports.world = function () {
     console.log('hello world');
-}
+};
 
 exports.sub = function (a, b) {
     console.log(a + b);
-}
+};
