ab367a12e8a0d6e1ba743fc11475b70a37e00684
zhentaoo <1904974327@qq.com>
2015-07-05 14:53:07 +0800

    xx

--- mongoose/mongo.js
+++ mongoose/mongo.js
@@ -1,11 +1,6 @@
 // mongoose 链接
 var mongoose = require('mongoose');
-var db = mongoose.createConnection('mongodb://127.0.0.1:27017/NodeJS');
-
-// 链接错误
-db.on('error', function (error) {
-    console.log(error);
-});
+mongoose.connect('mongodb://127.0.0.1:27017/NodeJS');
 
 // Schema 结构
 var mongooseSchema = new mongoose.Schema({
@@ -16,18 +11,12 @@ var mongooseSchema = new mongoose.Schema({
     age: {type: Number}
 });
 
-// 添加 mongoose 实例方法
-mongooseSchema.methods.findbyusername = function (username, callback) {
-    return this.model('mongoose').find({username: username}, callback);
-};
+var mongooseModel = mongoose.model('mongoose', mongooseSchema);
 
-// 添加 mongoose 静态方法，静态方法在Model层就能使用
-mongooseSchema.statics.findbytitle = function (title, callback) {
-    return this.model('mongoose').find({title: title}, callback);
-};
-
-// model
-var mongooseModel = db.model('mongoose', mongooseSchema);
+/*模型查询*/
+mongooseModel.find({name: 'leo'}, function (err, docs) {
+    console.log(docs);
+});
 
 // 增加记录 基于 entity 操作
 var doc = {username: 'emtity_demo_username', title: 'emtity_demo_title', content: 'emtity_demo_content'};
@@ -39,8 +28,8 @@ mongooseEntity.save(function (error) {
     } else {
         console.log('saved OK!');
     }
-    // 关闭数据库链接
-    db.close();
+    //关闭数据库链接
+    mongooseModel.close();
 });
 
 //// 增加记录 基于model操作
