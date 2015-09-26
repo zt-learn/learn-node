37237624eb3cc35fa684ecf42932d7c67411cb29
zhentaoo <1904974327@qq.com>
2015-09-26 20:52:13 +0800

    test

--- lodash/index.js
+++ lodash/index.js
@@ -2,13 +2,20 @@
  * Created by zhentaoo on 15/9/26.
  */
 var _ = require('lodash');
-var moment=require('moment');
+var moment = require('moment');
 
-var a=_.defaults({startTime:2}, {
+//将多个json合并,属性重复的合并,取前面的
+var a = _.defaults({
+  startTime: 2,
+  a:111,
+  b:111
+}, {
   startTime: moment().add(-7, 'days').format('YYYY-MM-DD HH:mm:ss'),
   endTime: moment().format('YYYY-MM-DD HH:mm:ss'),
   limit: 30
 }, {
-  a:1
+  a: 333
+}, {
+  b: 4444
 });
 console.log(a);
