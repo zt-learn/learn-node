a41489fa254e23d08307a40c3653d7d99f8cefe1
zhentaoo <1904974327@qq.com>
2015-11-17 13:10:16 +0800

    重要提交：Q.async es6 promise写法

---
+++ q/async.js
@@ -0,0 +1,32 @@
+/**
+ * Created by zhentaoo on 15/11/17.
+ */
+var Q = require('q');
+var fs = require('fs');
+
+
+var readFile = function (path) {
+  var deferred = Q.defer();
+
+  fs.readFile(path, function (err, result) {
+    if (err) {
+      deferred.reject(err);
+    } else {
+      deferred.resolve(result);
+    }
+  });
+
+  return deferred.promise;
+};
+
+Q.async(function *() {
+  var data = parseInt(yield readFile('test1.txt'));
+
+  var a = data + 20;
+
+  var data2 = parseInt(yield readFile('test2.txt'));
+
+  var b = data + data2;
+
+  console.log(a, b);
+})();
