e755dc4f1ae006d00bd66e4a4fcc645641f895bf
zhentaoo <zhentao.li@ele.me>
2016-06-03 14:26:58 +0800

    delete something

--- modules/teacher.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by leo on 2015/5/12.
- */
-function add(teacher) {
-    console.log('add teacher' + teacher);
-}
-
-exports.add = add;
--- str/index.js
+++
@@ -1,2 +0,0 @@
-var str='asdfadsf';
-console.log(str.substring(0,4));
